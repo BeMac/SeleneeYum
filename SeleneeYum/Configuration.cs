ad2b3192704398215d40874d2c8ba35a52f9f7fa
Brian McLaughlin <brainsbox@outlook.com>
2019-04-27 18:22:50 -0500

    Add project files.

---
+++ SeleneeYum/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleneeYum
+{
+    public class Configuration
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ckbxTermsOfService { get; set; }
+        public string btnStart { get; set; }
+    }
+}
---
+++ SeleneeYum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleneeYum
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            var driverDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            
+            using (var driver = new ChromeDriver(driverDirectory))
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+
+                GoToGoogle(driver);
+
+                GoToLostPet(driver);
+            }
+        }
+
+        private static string GoToGoogle(IWebDriver driver)
+        {
+            string googleUrl = "https://www.google.com/";
+            driver.Navigate().GoToUrl(googleUrl);
+            return googleUrl;
+        }
+
+        private static void GetConfigs()
+        {
+
+            //var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Config.json", true, true);
+            //Configuration = builder.Build();
+            //var happy = Configuration.GetSection("ConnectionStrings");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var happy = config["name"];
+
+        }
+        private static string GoToLostPet(IWebDriver driver)
+        {
+            return string.Empty;
+        }
+
+        private string FillInDetails()
+        {
+            return string.Empty;
+        }
+    }
+}
